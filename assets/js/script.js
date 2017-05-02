bafb9a12ea07fc681ef6c0027f6402403a0d8849
SGS Sandhu <contact@sgssandhu.com>
2017-05-02 22:25:55 +0530

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,42 @@
 $(document).ready(function(){
      var htmlContent = `<div class="auto-link-sidebar">
-	<button class="auto-linkedin-withdraw">Withdraw</button>
-	<button class="auto-linkedin-connect">Connect</button>
-	<button class="auto-linkedin-connect-searched">Connect Searched</button>
-	<button class="auto-linkedin-send-message">Send Message</button>
+	<button class="auto-linkedin-button auto-linkedin-withdraw">Withdraw</button>
+	<button class="auto-linkedin-button auto-linkedin-connect">Connect</button>
+	<button class="auto-linkedin-button auto-linkedin-connect-searched">Connect Searched</button>
+	<button class="auto-linkedin-button auto-linkedin-send-message">Send Message</button>
 </div>
 
 <style type="text/css">
-	.auto-link-sidebar{
-		width: 4%;
-		height: 100%;
-		position: fixed;
-		top: 0;
-		left: 0;
-		background-color: red;
-		z-index: 999;
-	}
-        .auto-linkedin-withdraw, .auto-linkedin-connect, .auto-linkedin-connect-searched, .auto-linkedin-send-message{
-		background-color: #e4e4e4;
-		color: #000;
-		padding: 10px;
-		margin-top: 15%;
-	}
+.application-outlet {
+    margin-left: 120px;
+}
+.auto-link-sidebar {
+	width: 120px;
+	height: 100%;
+	position: fixed;
+	top: 52px;
+	left: 0;
+	background-color: #283e4a;
+	z-index: 999;
+}
+.auto-linkedin-button{
+    background-color: #ffffff;
+    color: #0084bf;
+    padding: 8px 6px;
+    margin: 10px auto 10px auto;
+    width: 90%;
+    display: block;
+    font-size: 16px;
+    line-height: 22px;
+	-webkit-transition: all 200ms ease-in-out;
+    -moz-transition: all 200ms ease-in-out;
+    -o-transition: all 200ms ease-in-out;
+    transition: all 200ms ease-in-out;
+}
+.auto-linkedin-button:hover{
+    background-color: #0077b5;
+    color: #ffffff;
+}
 </style>`;
      $('body').append(htmlContent);
      $('.auto-linkedin-withdraw').click(function(){
