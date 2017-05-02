abdef1674d351c9e09f55934bca995af64fa889b
SGS Sandhu <contact@sgssandhu.com>
2017-05-02 23:16:46 +0530

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,113 @@
 $(document).ready(function(){
-     var htmlContent = `<div class="auto-link-sidebar">
-	<button class="auto-linkedin-button auto-linkedin-withdraw">Withdraw</button>
-	<button class="auto-linkedin-button auto-linkedin-connect">Connect</button>
-	<button class="auto-linkedin-button auto-linkedin-connect-searched">Connect Searched</button>
-	<button class="auto-linkedin-button auto-linkedin-send-message">Send Message</button>
-</div>
+     var htmlContent = `
+	 <div class="auto-link-sidebar">
+		<div class="auto-linkedin-group">
+			<h3 class="auto-linkedin-title" title="Manage invitations">
+			Manage invitations
+			<a href="https://www.linkedin.com/mynetwork/invitation-manager/">GO</a>
+			</h3>
+			
+			<h4 class="auto-linkedin-title" title="Manage invitations">
+			Received
+			<a href="https://www.linkedin.com/mynetwork/invitation-manager/">GO</a>
+			</h4>
+			<button class="auto-linkedin-button auto-linkedin-invitation-manager-ignore">Ignore</button>
+			<button class="auto-linkedin-button auto-linkedin-invitation-manager-accept">Accept</button>
+			<h4 class="auto-linkedin-title" title="Manage invitations">
+			Sent
+			<a href="https://www.linkedin.com/mynetwork/invitation-manager/sent/">GO</a>
+			</h4>
+			<button class="auto-linkedin-button auto-linkedin-invitation-manager-withdraw">Withdraw</button>
+		</div>
+		
+		<button class="auto-linkedin-button auto-linkedin-connect">Connect</button>
+		<button class="auto-linkedin-button auto-linkedin-connect-searched">Connect Searched</button>
+		<button class="auto-linkedin-button auto-linkedin-send-message">Send Message</button>
+	</div>
 
-<style type="text/css">
-.application-outlet {
-    margin-left: 120px;
-}
-.auto-link-sidebar {
-	width: 120px;
-	height: 100%;
-	position: fixed;
-	top: 52px;
-	left: 0;
-	background-color: #283e4a;
-	z-index: 999;
-}
-.auto-linkedin-button{
-    background-color: #ffffff;
-    color: #0084bf;
-    padding: 8px 6px;
-    margin: 10px auto 10px auto;
-    width: 90%;
-    display: block;
-    font-size: 16px;
-    line-height: 22px;
-	-webkit-transition: all 200ms ease-in-out;
-    -moz-transition: all 200ms ease-in-out;
-    -o-transition: all 200ms ease-in-out;
-    transition: all 200ms ease-in-out;
-}
-.auto-linkedin-button:hover{
-    background-color: #0077b5;
-    color: #ffffff;
-}
-</style>`;
-     $('body').append(htmlContent);
-     $('.auto-linkedin-withdraw').click(function(){
-	var loopInterval = setInterval(function(){
-		var allElements = $('.mn-person-card__person-btn-ext').length;
-		if(allElements == 0){
-			clearInterval(loopInterval);
-		}else{
-			$('.mn-person-card__person-btn-ext').each(function(){
-				$(this).click();
-			});
-		}
-	},5000);
-     });
+	<style type="text/css">
+	.application-outlet {
+		margin-left: 120px;
+	}
+	.auto-link-sidebar {
+		width: 120px;
+		height: 100%;
+		position: fixed;
+		top: 52px;
+		left: 0;
+		background-color: #283e4a;
+		z-index: 999;
+	}
+	.auto-linkedin-button{
+		background-color: #ffffff;
+		color: #0084bf;
+		padding: 8px 6px;
+		margin: 10px auto 10px auto;
+		width: 90%;
+		display: block;
+		font-size: 16px;
+		line-height: 22px;
+		-webkit-transition: all 200ms ease-in-out;
+		-moz-transition: all 200ms ease-in-out;
+		-o-transition: all 200ms ease-in-out;
+		transition: all 200ms ease-in-out;
+	}
+	.auto-linkedin-button:hover{
+		background-color: #0077b5;
+		color: #ffffff;
+	}
+	</style>`;
+	mn-invitation-list
+	
+	$('body').append(htmlContent);
+	
+	//Ignore
+	$('.auto-linkedin-invitation-manager-ignore').click(function(){
+		var invitation_manager_ignore = setInterval(function(){
+			var items = $('.mn-invitation-list .button-tertiary-medium-muted');
+			if(items.length == 0){
+				clearInterval(invitation_manager_ignore);
+			}else{
+				items.each(function(){
+					$(this).click();
+				});
+			}
+		},5000);
+	});
+	//Accept
+	$('.auto-linkedin-invitation-manager-accept').click(function(){
+		var invitation_manager_accept = setInterval(function(){
+			var items = $('.mn-invitation-list .button-secondary-medium');
+			if(items.length == 0){
+				clearInterval(invitation_manager_accept);
+			}else{
+				items.each(function(){
+					$(this).click();
+				});
+			}
+		},5000);
+	});
+	//Withdraw
+	$('.auto-linkedin-invitation-manager-withdraw').click(function(){
+		var invitation_manager_withdraw = setInterval(function(){
+			var items = $('.mn-invitation-list .button-tertiary-medium-muted');
+			if(items.length == 0){
+				clearInterval(invitation_manager_withdraw);
+			}else{
+				items.each(function(){
+					$(this).click();
+				});
+			}
+		},5000);
+	});
+	
+	
+	
+	
+	
+	
+
+	 
      $('.auto-linkedin-connect').click(function(){
 	   var connectInterval = setInterval(function(){
 		var allElements = $('.mn-pymk-list .mn-person-card__person-btn-ext').length;
@@ -66,6 +123,8 @@ $(document).ready(function(){
 		}
 	   },5000);  
      });
+	 
+	 
      $('.auto-linkedin-connect-searched').click(function(){
 	  
 	var buttonsLength = $('.search-result__occluded-item').length;
