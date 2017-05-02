d873853f295c0132e52653edf38c38034b18adbb
Rahul Sharma <rahulsharma841990@outlook.com>
2017-05-02 14:22:09 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
-     var htmlContent
-     $.get('https://rawgit.com/oxosolutions/auto-linkedin/master/assets/js/html.html', function(result){
-          htmlContent = result;
-     });
+     var htmlContet = `<div class="sidebar">
+	<button class="withdraw">Withdraw</button>
+	<button class="connect">Connect</button>
+</div>
+
+<style type="text/css">
+	.sidebar{
+		width: 4%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		background-color: red;
+	}
+</style>`;
      $('body').append(htmlContent);
 });
