16375bf93707ad5492418b9a095661a979a79a7c
Rahul Sharma <rahulsharma841990@outlook.com>
2017-05-02 14:31:14 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
 $(document).ready(function(){
-     var htmlContent = `<div class="sidebar">
+     var htmlContent = `<div class="auto-link-sidebar">
 	<button class="withdraw">Withdraw</button>
 	<button class="connect">Connect</button>
 </div>
 
 <style type="text/css">
-	.sidebar{
+	.auto-link-sidebar{
 		width: 4%;
 		height: 100%;
 		position: absolute;
 		top: 0;
 		left: 0;
 		background-color: red;
-		z-index = 999;
+		z-index: 999;
+	}
+        .connect, .withdraw{
+		background-color: #949494;
+		color: #000;
+		padding: 10px;
 	}
 </style>`;
      $('body').append(htmlContent);
